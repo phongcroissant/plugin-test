fe29d0453112d8bb580354ec221d20114321ac5d
phongnguyen <phongnguyen25000@gmail.com>
2026-02-09 15:29:55 +0100

    add:test add name button

--- desktop/php/plugin-test.php
+++ desktop/php/plugin-test.php
@@ -17,7 +17,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			<div class="cursor eqLogicAction logoPrimary" data-action="add">
 				<i class="fas fa-plus-circle"></i>
 				<br>
-				<span>{{Ajouter}}</span>
+				<span>{{Ajouter test}}</span>
 			</div>
 			<div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
 				<i class="fas fa-wrench"></i>
