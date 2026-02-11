53d9300179af5fcc19fec516681df72307750e73
phongnguyen <phongnguyen25000@gmail.com>
2026-02-11 14:19:45 +0100

    fix:command on and off 2

--- core/ajax/ImactPlugin.ajax.php
+++ core/ajax/ImactPlugin.ajax.php
@@ -62,7 +62,7 @@ try {
       $cmdOn->setType('action');
       $cmdOn->setLogicalId('on');
       $cmdOn->setSubType('other');
-      $cmdOn->setValue("Etat");
+      $cmdOn->setValue($cmd->getId());
       $cmdOn->setConfiguration('updateCmdId', $cmd->getId());
       $cmdOn->setConfiguration('updateCmdToValue', 1);
       $cmdOn->setIsVisible(1);
