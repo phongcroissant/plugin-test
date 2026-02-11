a059e1ec60045cc3b5ae106f34708adaf88a0288
phongnguyen <phongnguyen25000@gmail.com>
2026-02-11 14:33:15 +0100

    re-add:cmd on

--- core/ajax/ImactPlugin.ajax.php
+++ core/ajax/ImactPlugin.ajax.php
@@ -55,17 +55,17 @@ try {
       $cmd->setIsHistorized(1);
       $cmd->save();
 
-      // $cmdOn = new virtualCmd();
-      // $cmdOn->setName('On');
-      // $cmdOn->setEqLogic_id($virtual->getId());
-      // $cmdOn->setType('action');
-      // $cmdOn->setLogicalId('on');
-      // $cmdOn->setSubType('other');
-      // $cmdOn->setValue($cmd->getId());
-      // $cmdOn->setConfiguration('updateCmdId', $cmd->getId());
-      // $cmdOn->setConfiguration('updateCmdToValue', 1);
-      // $cmdOn->setIsVisible(1);
-      // $cmdOn->save();
+      $cmdOn = new virtualCmd();
+      $cmdOn->setName('On');
+      $cmdOn->setEqLogic_id($virtual->getId());
+      $cmdOn->setType('action');
+      $cmdOn->setLogicalId('on');
+      $cmdOn->setSubType('other');
+      $cmdOn->setValue($cmd->getId());
+      $cmdOn->setConfiguration('updateCmdId', $cmd->getId());
+      $cmdOn->setConfiguration('updateCmdToValue', 1);
+      $cmdOn->setIsVisible(1);
+      $cmdOn->save();
 
       // $cmdOff = new virtualCmd();
       // $cmdOff->setName('Off');
