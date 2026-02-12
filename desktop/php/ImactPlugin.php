d6b7b54238faa2314f07543cbc7eb9837267ef55
phongnguyen <phongnguyen25000@gmail.com>
2026-02-12 08:38:04 +0100

    re-add config

--- desktop/php/ImactPlugin.php
+++ desktop/php/ImactPlugin.php
@@ -30,45 +30,45 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<span>{{Configuration}}</span>
 			</div>
 		</div>
-		<!-- <legend><i class="fas fa-table"></i> {{Mes templates}}</legend> -->
+		<legend><i class="fas fa-table"></i> {{Mes templates}}</legend>
 		<?php
-		// if (count($eqLogics) == 0) {
-		// 	echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Template trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
-		// } else {
-		// Champ de recherche
-		// echo '<div class="input-group" style="margin:5px;">';
-		// echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
-		// echo '<div class="input-group-btn">';
-		// echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
-		// echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
-		// echo '</div>';
-		// echo '</div>';
-		// Liste des équipements du plugin
-		// 	echo '<div class="eqLogicThumbnailContainer">';
-		// 	foreach ($eqLogics as $eqLogic) {
-		// 		$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-		// 		echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
-		// 		echo '<img src="' . $eqLogic->getImage() . '"/>';
-		// 		echo '<br>';
-		// 		echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-		// 		echo '<span class="hiddenAsCard displayTableRight hidden">';
-		// 		echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
-		// 		echo '</span>';
-		// 		echo '</div>';
-		// 	}
-		// 	echo '</div>';
-		// }
+		if (count($eqLogics) == 0) {
+			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Template trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
+		} else {
+		Champ de recherche
+		echo '<div class="input-group" style="margin:5px;">';
+		echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
+		echo '<div class="input-group-btn">';
+		echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
+		echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
+		echo '</div>';
+		echo '</div>';
+		Liste des équipements du plugin
+			echo '<div class="eqLogicThumbnailContainer">';
+			foreach ($eqLogics as $eqLogic) {
+				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+				echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
+				echo '<img src="' . $eqLogic->getImage() . '"/>';
+				echo '<br>';
+				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+				echo '<span class="hiddenAsCard displayTableRight hidden">';
+				echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
+				echo '</span>';
+				echo '</div>';
+			}
+			echo '</div>';
+		}
 		?>
-		<!-- </div> -->
+		</div>
 		<!-- /.eqLogicThumbnailDisplay -->
 
 		<!-- Page de présentation de l'équipement -->
-		<!-- <div class="col-xs-12 eqLogic" style="display: none;"> -->
+		<div class="col-xs-12 eqLogic" style="display: none;">
 		<!-- barre de gestion de l'équipement -->
-		<!-- <div class="input-group pull-right" style="display:inline-flex;">
-				<span class="input-group-btn"> -->
+		<div class="input-group pull-right" style="display:inline-flex;">
+				<span class="input-group-btn">
 		<!-- Les balises <a></a> sont volontairement fermées à la ligne suivante pour éviter les espaces entre les boutons. Ne pas modifier -->
-		<!-- <a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i
+		<a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i
 							class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
 					</a><a class="btn btn-sm btn-default eqLogicAction" data-action="copy"><i
 							class="fas fa-copy"></i><span class="hidden-xs"> {{Dupliquer}}</span>
@@ -78,9 +78,9 @@ $eqLogics = eqLogic::byType($plugin->getId());
 							class="fas fa-minus-circle"></i> {{Supprimer}}
 					</a>
 				</span>
-			</div> -->
+			</div>
 		<!-- Onglets -->
-		<!-- <ul class="nav nav-tabs" role="tablist">
+		<ul class="nav nav-tabs" role="tablist">
 				<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab"
 						data-toggle="tab" data-action="returnToThumbnailDisplay"><i
 							class="fas fa-arrow-circle-left"></i></a></li>
@@ -88,13 +88,13 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
 				<li role="presentation"><a href="#commandtab" aria-controls="home" role="tab" data-toggle="tab"><i
 							class="fas fa-list"></i> {{Commandes}}</a></li>
-			</ul> -->
-		<!-- <div class="tab-content"> -->
-		<!-- Onglet de configuration de l'équipement -->
-		<!-- <div role="tabpanel" class="tab-pane active" id="eqlogictab"> -->
+			</ul>
+		<div class="tab-content">
+		Onglet de configuration de l'équipement
+		<div role="tabpanel" class="tab-pane active" id="eqlogictab">
 		<!-- Partie gauche de l'onglet "Equipements" -->
 		<!-- Paramètres généraux et spécifiques de l'équipement -->
-		<!-- <form class="form-horizontal">
+		<form class="form-horizontal">
 						<fieldset>
 							<div class="col-lg-6">
 								<legend><i class="fas fa-wrench"></i> {{Paramètres généraux}}</legend>
@@ -113,11 +113,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 										<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
 											<option value="">{{Aucun}}</option>
 											<?php
-											// $options = '';
-											// foreach ((jeeObject::buildTree(null, false)) as $object) {
-											// 	$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
-											// }
-											// echo $options;
+											$options = '';
+											foreach ((jeeObject::buildTree(null, false)) as $object) {
+												$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
+											}
+											echo $options;
 											?>
 										</select>
 									</div>
@@ -164,10 +164,10 @@ $eqLogics = eqLogic::byType($plugin->getId());
 										<input type="text" class="eqLogicAttr form-control inputPassword"
 											data-l1key="configuration" data-l2key="password">
 									</div>
-								</div> -->
+								</div> 
 		<!-- Exemple de champ de saisie du cron d'auto-actualisation avec assistant -->
 		<!-- La fonction cron de la classe du plugin doit contenir le code prévu pour que ce champ soit fonctionnel -->
-		<!-- <div class="form-group">
+		<div class="form-group">
 									<label class="col-sm-4 control-label">{{Auto-actualisation}}
 										<sup><i class="fas fa-question-circle tooltips"
 												title="{{Fréquence de rafraîchissement des commandes infos de l'équipement}}"></i></sup>
@@ -186,11 +186,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 										</div>
 									</div>
 								</div>
-							</div> -->
+							</div>
 
 		<!-- Partie droite de l'onglet "Équipement" -->
 		<!-- Affiche un champ de commentaire par défaut mais vous pouvez y mettre ce que vous voulez -->
-		<!-- <div class="col-lg-6">
+		<div class="col-lg-6">
 								<legend><i class="fas fa-info"></i> {{Informations}}</legend>
 								<div class="form-group">
 									<label class="col-sm-4 control-label">{{Description}}</label>
@@ -201,12 +201,12 @@ $eqLogics = eqLogic::byType($plugin->getId());
 								</div>
 							</div>
 						</fieldset>
-					</form> -->
-		<!-- </div> -->
-		<!-- /.tabpanel #eqlogictab-->
+					</form>
+		</div>
+		<!-- /.tabpanel #eqlogictab -->
 
 		<!-- Onglet des commandes de l'équipement -->
-		<!-- <div role="tabpanel" class="tab-pane" id="commandtab">
+		<div role="tabpanel" class="tab-pane" id="commandtab">
 					<a class="btn btn-default btn-sm pull-right cmdAction" data-action="add" style="margin-top:5px;"><i
 							class="fas fa-plus-circle"></i> {{Ajouter une commande}}</a>
 					<br><br>
@@ -225,8 +225,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
 							<tbody>
 							</tbody>
 						</table>
-					</div> -->
-	</div><!-- /.tabpanel #commandtab-->
+					</div>
+	</div><!-- /.tabpanel #commandtab -->
 
 </div><!-- /.tab-content -->
 </div><!-- /.eqLogic -->
